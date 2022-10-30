47955212e2bc2e058825ce92a95d00ebdfe67114
biela000 <jak.biela000@gmail.com>
2022-10-30 14:08:38 +0100

    Start the 8th section of the NodeJS course.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_LOCAL;
+mongoose.connect(DB).then((con) => {
+  console.log(con.connections);
+  console.log('DB connection successful!');
+});
 
 const app = require('./app');
 
